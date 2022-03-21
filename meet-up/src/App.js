1a7ee1eabdf6651883bf019d8766bbcacb5e0de1
ravi <d.raviteja@hotmail.com>
2022-03-21 22:58:20 +0530

    first-commmit

---
+++ meet-up/src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
+function App() {
+  //localhost:3000/path
+  return(
+  <div>
+        <Route path='/'>
+          <AllMeetupsPage/>
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage/>
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage/>
+        </Route>
+  </div>
+  );
+}
+
+export default App;
